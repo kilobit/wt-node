dfa8e113ddecc0e19e809404c08a971636557d1e
Michelle Schuster <schuster.mb@gmail.com>
2014-02-06 09:27:23 -0700

    added the option to parse the parameters before calling the route's function

--- router.js
+++ router.js
@@ -18,31 +18,37 @@
 
 var url = require("url");
 var logger = require("./logger");
+var qsparser = require('querystring');
 
 var router = {};
 
 (function(){
 
     router.routes = {};
+    router.prerunChain = [];
 
     router.setDefaultHandler = function(handler) {
 	this.default_handler = handler;
     }
 
     router.setDefaultRouteHandler = function(path, handler) {
-	
+
 	// Get existing route entries or create new ones.
 	var route = (this.routes[path]) ? this.routes[path] : {};
 
 	// Set the default handler on the route.
 	route.default_handler = handler;
-	
+
 	// Add the route to the routes.
 	this.routes[path] = route;
     }
 
+    router.addPreHandling = function(fname) {
+    	router.prerunChain.push(fname);
+    }
+
     router.setHandler = function(method, path, handler) {
-	
+
 	// Get existing route entries or create new ones.
 	var route = (this.routes[path]) ? this.routes[path] : {};
 	var handlers = (route.handlers) ? route.handlers : {};
@@ -62,6 +68,53 @@ var router = {};
 	this.routes = routes;
     }
 
+	router.runBefore = function(request, response, data, routeHandler) {
+
+		var next = function() {
+			if(router.prerunChain.length === 0) {
+				return routeHandler(request, response, data);
+			}
+			var now = router.prerunChain.shift();
+			if( typeof now != 'function' ) {
+				throw new Error(now.toString() + " is not a function!");
+			}
+
+			return now(request, response, data, next);
+		};
+		next();
+	};
+
+    router.parsePostData = function(request, response, data, next) {
+		if (request.method == "GET") {
+			return next();
+		}
+		var postData = "";
+		request.setEncoding("utf8");
+		request.on("data", function(data) {
+			postData += data;
+		});
+		request.on("end", function() {
+			// Pass control to the routed method.
+			if (postData.match(/<(.*)>(.*)<\/(.*)>/)) {
+				return routeMethod(request, response, postData);
+			}
+			data = qsparser.parse(postData);
+			request.data = data;
+			return next();
+		});
+    };
+    router.parseGetParameters = function(request, response, data, next) {
+		var params = {};
+		// add get params
+		request.url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+			params[key] = value;
+		});
+
+		request.query = params;
+		data = data;
+		return next();
+	};
+
     // Convenience methods for setting handlers.
     router.get		= function(path, handler) { return this.setHandler('GET', path, handler); }
     router.post		= function(path, handler, data) { return this.setHandler('POST', path, handler); }
@@ -73,13 +126,13 @@ var router = {};
     // Route requests based on the routes list.
     router.route = function(request, response, data) {
 
-	
+
 	// Parse the request uri
 	uri = url.parse(request.url);
 
 	// Loop over the routes
 	for(var route in this.routes) {
-	    
+
 	    // Check for a match.
 	    match = this.routes[route].re.exec(uri.pathname);
 	    if(match) {
@@ -88,7 +141,11 @@ var router = {};
 		method = this.routes[route].handlers && this.routes[route].handlers[request.method.toUpperCase()];
 		if(method) {
 		    // Pass control to the routed method.
-		    return method(request, response, data);
+		    if(router.prerunChain.length > 0) {
+		    	return router.runBefore(request, response, data, method);
+		    } else {
+		    	return method(request, response, data);
+		    }
 		}
 
 		// Try the default route handler
@@ -110,11 +167,11 @@ var router = {};
     }
 
     NotFoundHandler = function(request, response) {
-	
+
 	response.writeHead(500, {'Content-Type': 'text/html'});
 
 	response.write("<html>\n\t<head>\n\t<title>404 Not Found</title>\n\t</head>\n");
-	response.write("<body>\n\t<h1>Not Found</h1>\n\t" + 
+	response.write("<body>\n\t<h1>Not Found</h1>\n\t" +
 		       "<p>The resource you requested, " + request.url +
 		       ", was not found on this server.<\p>\n</body>");
 
@@ -147,3 +204,5 @@ for(var name in router) {
 //         return this;
 //     }
 // });
+
+
